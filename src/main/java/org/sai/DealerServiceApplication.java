379763543525dc1c2eb927a39db57722ab98bad7
Sai <sai@madhavs-mbp.home>
2020-09-16 00:36:14 -0500

    starting of project

---
+++ src/main/java/org/sai/DealerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerServiceApplication.class, args);
+	}
+
+}
